1ff36dbb04d55f0437593f643ebd3cd264e5a81a
Hlovegood <hlovegood14@gmail.com>
2026-04-14 10:57:44 +0200

    feat: Changed Pictures in Carousel

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Signin.css';
 
-import img1 from '../assets/Imgs/Carousel-img (1).png';
 import img2 from '../assets/Imgs/Carousel-img (2).png';
 import img3 from '../assets/Imgs/Carousel-img (3).png';
 import img4 from '../assets/Imgs/Carousel-img (4).png';
-import img5 from '../assets/Imgs/Carousel-img (5).png';
 import img6 from '../assets/Imgs/Carousel-img (6).png';
-import img7 from '../assets/Imgs/Carousel-img (7).png';
-import img8 from '../assets/Imgs/Carousel-img (8).png';
-import img9 from '../assets/Imgs/Carousel-img (9).png';
+import img10 from '../assets/Imgs/Carousel-img (10).png';
+import img12 from '../assets/Imgs/Carousel-img (12).png';
+import img13 from '../assets/Imgs/Carousel-img (13).png';
+
 
 import senetIcon from '../assets/icons/Senet icon.png';
 import googleIcon from '../assets/icons/Google Icon.png';
@@ -18,15 +17,15 @@ import appleIcon from '../assets/icons/Apple Icon.png';
 import facebookIcon from '../assets/icons/Facebook Icon.png';
 
 const carouselImages = [
-  { id: 1, src: img1 },
-  { id: 2, src: img2 },
-  { id: 3, src: img3 },
-  { id: 4, src: img4 },
-  { id: 5, src: img5 },
-  { id: 6, src: img6 },
-  { id: 7, src: img7 },
-  { id: 8, src: img8 },
-  { id: 9, src: img9 },
+  { id: 1, src: img2 },
+  { id: 2, src: img3 },
+  { id: 3, src: img4 },
+  { id: 4, src: img6 },
+  { id: 5, src: img10 },
+  { id: 6, src: img12 },
+  { id: 7, src: img13 },
+
+
 ];
 
 export default function Signin() {
@@ -54,7 +53,7 @@ export default function Signin() {
         <div className="full-screen-splash">
           <div className="splash-bundle">
             <img src={senetIcon} alt="Logo" className="splash-logo-animated" />
-            <h1 className="splash-text-animated">SENET</h1>
+            <h1 className="splash-text-animated">Senet</h1>
           </div>
         </div>
       )}
